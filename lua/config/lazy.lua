1edd6cfd2adf980a2530b1dcc115b51b62d6e8a1
TheCrowd <747531141@qq.com>
2025-01-21 14:18:28 +1100

    add more plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,6 +50,7 @@ require("lazy").setup({
     },
   },
 })
+
 require("copilot").setup({
   suggestion = { enabled = false },
   panel = { enabled = false },
@@ -62,18 +63,5 @@ local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
-
-local cmp = require("cmp")
-cmp.setup({
-  mapping = {
-    ["<Tab>"] = vim.schedule_wrap(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        fallback()
-      end
-    end),
-  },
-})
 
 require("lspconfig").kotlin_language_server.setup({})
